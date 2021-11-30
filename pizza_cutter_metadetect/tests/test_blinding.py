358bc6e65e0bcd52845ee2f6fc2065cca7b6c7c8
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-11-30 12:55:45 -0500

    ENH remove masking code (#46)
    * ENH remove masking code
    
    * ENH config changes for latest mdet

--- pizza_cutter_metadetect/tests/test_blinding.py
+++
@@ -1,6 +0,0 @@
-from ..blinding import generate_blinding_factor
-
-
-def test_generate_blinding_factor():
-    # this test ensures the code does not change so when we unblind it works
-    assert generate_blinding_factor("blah") == 1.033477298
